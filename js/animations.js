19366cb91e7f34eef442ffe7b23e15fc768841c4
Abdu-net <99261556+AbduGithb@users.noreply.github.com>
2025-12-16 12:26:34 +0300

    add file initial-page

---
+++ js/animations.js
@@ -0,0 +1,7 @@
+
+        setTimeout(function() {
+            document.getElementById('initial-page').classList.add('hide');
+            document.getElementById('hero-content').classList.add('show');
+        }, 3000); // الانتقال بعد 3 ثواني
+   
+            
